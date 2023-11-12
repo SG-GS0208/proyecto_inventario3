334a050279a52dfb4fbca5d0637e85c7af0c6150
SG-GS0208 <126899140+SG-GS0208@users.noreply.github.com>
2023-11-12 08:37:07 -0500

    Initial commit

--- app/src/main/java/com/example/proyecto_inventario/controlador/servicios/inventario/registroproductos.kt
+++ app/src/main/java/com/example/proyecto_inventario/controlador/servicios/inventario/registroproductos.kt
@@ -1,11 +1,17 @@
 package com.example.proyecto_inventario.controlador.servicios.inventario
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.navigation.fragment.findNavController
 import com.example.proyecto_inventario.R
+import com.example.proyecto_inventario.controlador.HomeProyect
+import com.example.proyecto_inventario.controlador.Productosapp
 import com.example.proyecto_inventario.databinding.FragmentRegistroproductosBinding
 
 class registroproductos : Fragment(R.layout.fragment_registroproductos) {
@@ -15,6 +21,34 @@ class registroproductos : Fragment(R.layout.fragment_registroproductos) {
         super.onViewCreated(view, savedInstanceState)
 
         bindingRegistroproductos = FragmentRegistroproductosBinding.bind(view)
+
+        bindingRegistroproductos.IBBotonCancelar.setOnClickListener {
+            mensajeCancelarProducto()
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            mensajeCancelarProducto()
+        }
     }
+    private fun mensajeCancelarProducto() {
+        val builder = AlertDialog.Builder(requireContext())
+
+        builder.setTitle("Cancelar Registro de Producto")
+        builder.setMessage("¿Estás seguro de que deseas cancelar el registro del producto?")
+
+        builder.setPositiveButton("Sí") { dialog, which ->
+            // Aquí colocas el código que deseas ejecutar al presionar "Sí"
+            // Por ejemplo, puedes cerrar el fragmento o realizar otra acción.
+            startActivity(Intent(requireContext(), HomeProyect::class.java))
+        }
+
+        builder.setNegativeButton("No") { dialog, which ->
+            // Aquí colocas el código que deseas ejecutar al presionar "No"
+        }
+
+
+
+        builder.show()
+    }
+
 
 }
